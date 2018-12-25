2405310e48358448eea5f4b13381ff77bc45c1b4
dmiwer <dmiwer@gmail.com>
2018-12-25 09:38:55 +0300

    MinesweeperGame 6/16

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -2,6 +2,9 @@ package com.javarush.games.minesweeper;
 
 import com.javarush.engine.cell.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MinesweeperGame extends Game {
     private static final int SIDE = 9;
     private GameObject[][] gameField = new GameObject[SIDE][SIDE];
@@ -23,5 +26,31 @@ public class MinesweeperGame extends Game {
                 if (itIsMine) countMinesOnField++;
             }
         }
+        countMineNeighbors();
+    }
+
+    private void countMineNeighbors() {
+        for (GameObject[] gameObjects : gameField) {
+            for (GameObject gameObject : gameObjects) {
+                if (!gameObject.isMine){
+                    for (GameObject neighbor : getNeighbors(gameObject)) {
+                        if (neighbor.isMine) gameObject.countMineNeighbors++;
+                    }
+                }
+            }
+        }
+    }
+
+    private List<GameObject> getNeighbors(GameObject gameObject) {
+        List<GameObject> result = new ArrayList<>();
+        for (int dx = -1; dx < 2; dx++) {
+            for (int dy = -1; dy < 2; dy++) {
+                if (dx == 0 && dy == 0) continue;
+                if (gameObject.x + dx < 0 || gameObject.x + dx == SIDE) continue;
+                if (gameObject.y + dy < 0 || gameObject.y + dy == SIDE) continue;
+                result.add(gameField[gameObject.y + dy][gameObject.x + dx]);
+            }
+        }
+        return result;
     }
 }
