17f42728d6de51d4d1a3d4e3cc22742af727e839
dmiwer <dmiwer@gmail.com>
2018-12-25 08:59:12 +0300

    MinesweeperGame 4/16

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -4,10 +4,21 @@ import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
     private static final int SIDE = 9;
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
 
     @Override
     public void initialize() {
         super.initialize();
         setScreenSize(SIDE, SIDE);
+        createGame();
+    }
+
+    private void createGame() {
+        for (int x = 0; x < SIDE; x++) {
+            for (int y = 0; y < SIDE; y++) {
+                setCellColor(x, y, Color.ORANGE);
+                gameField[y][x] = new GameObject(x, y);
+            }
+        }
     }
 }
