564bc739513de0c2c0c3375953948a9e0e6fa409
dmiwer <dmiwer@gmail.com>
2018-12-27 10:09:22 +0300

    SnakeGame 6/20

--- 8.Games/src/com/javarush/games/snake/Snake.java
+++ 8.Games/src/com/javarush/games/snake/Snake.java
@@ -1,14 +1,28 @@
 package com.javarush.games.snake;
 
+import com.javarush.engine.cell.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
+    private static final String HEAD_SIGN = "\uD83D\uDC7E";
+    private static final String BODY_SIGN = "\u26AB";
     private List<GameObject> snakeParts = new ArrayList<>();
 
     public Snake(int x, int y) {
         snakeParts.add(new GameObject(x, y));
         snakeParts.add(new GameObject(x + 1, y));
         snakeParts.add(new GameObject(x + 2, y));
+    }
+
+    public void draw(Game game) {
+        for (int i = 0; i < snakeParts.size(); i++) {
+            GameObject gObj = snakeParts.get(i);
+            if (i == 0)
+                game.setCellValue(gObj.x, gObj.y, HEAD_SIGN);
+            else
+                game.setCellValue(gObj.x, gObj.y, BODY_SIGN);
+        }
     }
 }
--- 8.Games/src/com/javarush/games/snake/SnakeGame.java
+++ 8.Games/src/com/javarush/games/snake/SnakeGame.java
@@ -5,6 +5,7 @@ import com.javarush.engine.cell.*;
 public class SnakeGame extends Game {
     public static final int WIDTH = 15;
     public static final int HEIGHT = 15;
+    private Snake snake;
 
     @Override
     public void initialize() {
@@ -13,15 +14,17 @@ public class SnakeGame extends Game {
         createGame();
     }
 
-    private void drawScene(){
+    private void drawScene() {
         for (int x = 0; x < WIDTH; x++) {
             for (int y = 0; y < HEIGHT; y++) {
                 setCellColor(x, y, Color.DARKSEAGREEN);
             }
         }
+        snake.draw(this);
     }
 
-    private void createGame(){
+    private void createGame() {
+        snake = new Snake(WIDTH / 2, HEIGHT / 2);
         drawScene();
     }
 }
