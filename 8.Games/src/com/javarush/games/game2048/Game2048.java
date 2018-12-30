5aaa9f6db0205efbec2d289187f809221196cc49
dmiwer <dmiwer@gmail.com>
2018-12-30 15:58:38 +0300

    Game2048 4/18

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -10,9 +10,18 @@ public class Game2048 extends Game {
     public void initialize() {
         setScreenSize(SIDE, SIDE);
         createGame();
+        drawScene();
     }
 
-    private void createGame(){
+    private void drawScene() {
+        for (int x = 0; x < SIDE; x++) {
+            for (int y = 0; y < SIDE; y++) {
+                setCellColor(x, y, Color.RED);
+            }
+        }
+    }
+
+    private void createGame() {
 
     }
 }
