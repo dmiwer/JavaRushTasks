fd09079e7a9b2ab810eddaa5c8aa34c357d658dc
dmiwer <dmiwer@gmail.com>
2018-12-31 11:52:19 +0300

    Game2048 15/18

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -17,67 +17,12 @@ public class Game2048 extends Game {
         drawScene();
     }
 
-    private void createNewNumber() {
-        int x, y;
-        if (getMaxTileValue() == 2048) win();
-        do {
-            x = getRandomNumber(SIDE);
-            y = getRandomNumber(SIDE);
-        } while (gameField[y][x] != 0);
-        if (getRandomNumber(10) == 9)
-            gameField[y][x] = 4;
-        else
-            gameField[y][x] = 2;
-    }
-
-    private boolean compressRow(int[] row) {
-        boolean res = false;
-        for (int i = 0; i < SIDE - 1; i++) {
-            for (int j = i + 1; j < SIDE; j++) {
-                if (row[i] == 0 && row[j] != 0) {
-                    int t = row[i];
-                    row[i] = row[j];
-                    row[j] = t;
-                    res = true;
-                }
-            }
-        }
-        return res;
-    }
-
-    private boolean mergeRow(int[] row) {
-        boolean res = false;
-        for (int i = 0; i < SIDE - 1; i++) {
-            if (row[i] != 0 && row[i] == row[i + 1]) {
-                row[i] *= 2;
-                row[i + 1] = 0;
-                res = true;
-            }
-        }
-        return res;
-    }
-
-    private boolean canUserMove() {
-        for (int x = 0; x < SIDE; x++) {
-            for (int y = 0; y < SIDE; y++) {
-                if (gameField[y][x] == 0) return true;
-                if (x < SIDE - 1 && gameField[y][x] == gameField[y][x + 1]) return true;
-                if (y < SIDE - 1 && gameField[y][x] == gameField[y + 1][x]) return true;
-            }
-        }
-        return false;
-    }
-
-    private void rotateClockwise() {
-        int[][] rotatedField = new int[SIDE][SIDE];
-        for (int i = 0; i < SIDE; i++)
-            for (int j = 0; j < SIDE; j++)
-                rotatedField[i][j] = gameField[SIDE - j - 1][i];
-        gameField = rotatedField;
-    }
-
     @Override
     public void onKeyPress(Key key) {
+        if (!canUserMove()) {
+            gameOver();
+            return;
+        }
         switch (key) {
             case LEFT:
                 moveLeft();
@@ -131,6 +76,65 @@ public class Game2048 extends Game {
         rotateClockwise();
         rotateClockwise();
         rotateClockwise();
+    }
+
+    private boolean compressRow(int[] row) {
+        boolean res = false;
+        for (int i = 0; i < SIDE - 1; i++) {
+            for (int j = i + 1; j < SIDE; j++) {
+                if (row[i] == 0 && row[j] != 0) {
+                    int t = row[i];
+                    row[i] = row[j];
+                    row[j] = t;
+                    res = true;
+                }
+            }
+        }
+        return res;
+    }
+
+    private boolean mergeRow(int[] row) {
+        boolean res = false;
+        for (int i = 0; i < SIDE - 1; i++) {
+            if (row[i] != 0 && row[i] == row[i + 1]) {
+                row[i] *= 2;
+                row[i + 1] = 0;
+                res = true;
+            }
+        }
+        return res;
+    }
+
+    private boolean canUserMove() {
+        for (int x = 0; x < SIDE; x++) {
+            for (int y = 0; y < SIDE; y++) {
+                if (gameField[y][x] == 0) return true;
+                if (x < SIDE - 1 && gameField[y][x] == gameField[y][x + 1]) return true;
+                if (y < SIDE - 1 && gameField[y][x] == gameField[y + 1][x]) return true;
+            }
+        }
+        return false;
+    }
+
+    private void createNewNumber() {
+        int x, y;
+        if (getMaxTileValue() == 2048) win();
+        do {
+            x = getRandomNumber(SIDE);
+            y = getRandomNumber(SIDE);
+        } while (gameField[y][x] != 0);
+        if (getRandomNumber(10) == 9)
+            gameField[y][x] = 4;
+        else
+            gameField[y][x] = 2;
+    }
+
+    private void rotateClockwise() {
+        int[][] rotatedField = new int[SIDE][SIDE];
+        for (int i = 0; i < SIDE; i++)
+            for (int j = 0; j < SIDE; j++)
+                rotatedField[i][j] = gameField[SIDE - j - 1][i];
+        gameField = rotatedField;
     }
 
     private int getMaxTileValue() {
@@ -195,5 +199,10 @@ public class Game2048 extends Game {
     private void win() {
         isGameStopped = true;
         showMessageDialog(Color.NONE, "Win!", Color.DEEPPINK, 70);
+    }
+
+    private void gameOver(){
+        isGameStopped = true;
+        showMessageDialog(Color.NONE, "it`s over", Color.DEEPPINK, 70);
     }
 }
