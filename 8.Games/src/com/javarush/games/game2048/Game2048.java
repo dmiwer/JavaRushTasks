e07b3a3e1d42a41b535ec6e1c6c03565e1c1c541
dmiwer <dmiwer@gmail.com>
2018-12-31 11:31:23 +0300

    Game2048 12/18

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -97,12 +97,27 @@ public class Game2048 extends Game {
     }
 
     private void moveRight() {
+        rotateClockwise();
+        rotateClockwise();
+        moveLeft();
+        rotateClockwise();
+        rotateClockwise();
     }
 
     private void moveUp() {
+        rotateClockwise();
+        rotateClockwise();
+        rotateClockwise();
+        moveLeft();
+        rotateClockwise();
     }
 
     private void moveDown() {
+        rotateClockwise();
+        moveLeft();
+        rotateClockwise();
+        rotateClockwise();
+        rotateClockwise();
     }
 
     private Color getColorByValue(int value) {
