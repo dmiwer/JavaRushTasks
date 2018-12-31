c598afb2bd5b9974212344d661fd30022c452156
dmiwer <dmiwer@gmail.com>
2018-12-31 14:27:24 +0300

    MoonLanderGame 5/23

--- 8.Games/src/com/javarush/games/moonlander/GameObject.java
+++ 8.Games/src/com/javarush/games/moonlander/GameObject.java
@@ -1,13 +1,28 @@
 package com.javarush.games.moonlander;
 
+import com.javarush.engine.cell.*;
+
 public class GameObject {
     public double x;
     public double y;
+    public int width;
+    public int height;
     public int[][] matrix;
 
     public GameObject(double x, double y, int[][] matrix) {
         this.x = x;
         this.y = y;
         this.matrix = matrix;
+        width = matrix[0].length;
+        height = matrix.length;
+    }
+
+    public void draw(Game game){
+        if (matrix == null) return;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                game.setCellColor(x + (int)this.x, y + (int)this.y, Color.values()[matrix[y][x]]);
+            }
+        }
     }
 }
