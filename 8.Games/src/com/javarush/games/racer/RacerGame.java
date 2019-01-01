505c6bd7c3e55e5a91d14f390bebb269f23f3c53
dmiwer <dmiwer@gmail.com>
2019-01-01 20:07:49 +0300

    Racer 7/28

--- 8.Games/src/com/javarush/games/racer/RacerGame.java
+++ 8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -8,6 +8,7 @@ public class RacerGame extends Game {
     public static final int CENTER_X = WIDTH / 2;
     public static final int ROADSIDE_WIDTH = 14;
     private RoadMarking roadMarking;
+    private PlayerCar player;
 
     @Override
     public void initialize() {
@@ -25,12 +26,14 @@ public class RacerGame extends Game {
 
     private void createGame() {
         roadMarking = new RoadMarking();
+        player = new PlayerCar();
         drawScene();
     }
 
     private void drawScene() {
         drawField();
         roadMarking.draw(this);
+        player.draw(this);
     }
 
     private void drawField() {
